23f4c90cef8f9ff372a3cc244bca962d28b72063
JosueRCanto <josue.ramirezcanto@gmail.com>
2018-02-25 14:38:27 -0500

    Este

---
+++ src/CalculadorImpuestos.java
@@ -0,0 +1,16 @@
+//Ejercicio del libro
+//Ing Sistemas computacionales 4 Semestre Grupo A
+//Ramírez Canto Josué Emmanuel
+public class CalculadorImpuestos {
+	
+	
+	
+    public double BuscarAvaluoVehiculo(String mar, String lin, String mod){
+     return 0;
+    }
+
+   public double CalcularPago (String mar, String lin, String mod, boolean DescProntoPago, boolean DescServicioPublico,boolean DescTrasladoCuenta){
+        return 0;
+    }
+	
+}
